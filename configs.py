323fbe87c7d7001ad2618cd621f8450ebd7a0c7b
acecric11 <ACECRICKETGURU@GMAIL.COM>
2025-05-26 17:32:29 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -6,11 +6,11 @@
 from os import path, getenv
 
 class Config:
-    API_ID = int(getenv("API_ID", "21200698"))
+    API_ID = int(getenv("API_ID", "21200698" ))
     API_HASH = getenv("API_HASH", "e0d3c29dff9b449d710b1d6516be8673")
     BOT_TOKEN = getenv("7619169950:AAG_mY4wz_ndGky1MOvtZLYjnD39FoydkJc")
-    " , "Acecricketpro "" )
-    CHID = int(getenv("CHID",1001925074421 "")) # Make Bot Admin In This Channel
+    FSUB = getenve("FSUB" ,  "Acecricketpro" )
+    CHID = int(getenv("CHID","-1001925074421" )) # Make Bot Admin In This Channel
     # Admin Or Owner Id Below
     SUDO = list(map(int, getenv("SUDO", "7444596223").split()))
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://acecricketguru:<pcmVWShOJTGcz1kh>@cluster0.dxqtcyr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
