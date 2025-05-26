a7b55f7dc99b98dd4c08e33f4e584bfc46e403dd
acecric11 <ACECRICKETGURU@GMAIL.COM>
2025-05-27 00:04:42 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,18 +4,19 @@
 
 
 from os import path, getenv
+import os
 
 class Config:
-    API_ID = int(getenv("API_ID", "21200698" ))
-    API_HASH = getenv("API_HASH", "e0d3c29dff9b449d710b1d6516be8673")
-    BOT_TOKEN = getenv("7619169950:AAG_mY4wz_ndGky1MOvtZLYjnD39FoydkJc")
-    FSUB = getenve("FSUB" ,  "Acecricketpro" )
-    CHID = int(getenv("CHID" ,"-1001925074421" )) # Make Bot Admin In This Channel
-    # Admin Or Owner Id Below
-    SUDO = list(map(int, getenv("SUDO", "7444596223").split()))
-    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://acecricketguru:<pcmVWShOJTGcz1kh>@cluster0.dxqtcyr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-    
+    API_ID = int(os.getenv("API_ID", "21200698"))
+    API_HASH = os.getenv("API_HASH", "e0d3c29dff9b449d710b1d6516be8673")
+    BOT_TOKEN = os.getenv("BOT_TOKEN", "7619169950:AAG_mY4wz_ndGky1MOvtZLYjnD39FoydkJc")
+    FSUB = os.getenv("FSUB", "Acecricketpro")
+    CHID = int(os.getenv("CHID", "-1001925074421"))
+    SUDO = list(map(int, os.getenv("SUDO", "7444596223").split()))
+    MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://acecricketguru:<password>@cluster0.dxqtcyr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+
 cfg = Config()
+
 
 # Don't Remove Credit @Acecricketpro
 # Subscribe YouTube Channel For Amazing Bot @Acecricketpro
